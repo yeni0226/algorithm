bd1d1f48bc6c8a374aafafae91c8dc72d4b1912b
yeni0226 <yee6258@gmail.com>
2024-03-05 23:48:27 +0900

    message

---
+++ 백준 스터디/백준_10828/백준_10828/백준_10828.cpp
@@ -0,0 +1,57 @@
+﻿// 백준_10828.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	stack<int>s;
+	int n, x;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string a;
+		cin >> a;
+		if (a == "push") {
+			cin >> x;
+			s.push(x);
+		}
+		else if (a == "pop") {
+			if (s.size() == 0) {
+				cout << -1 << "\n";
+			}
+			else {
+				cout << s.top() << "\n";
+				s.pop();
+			}
+		}
+		else if (a == "size") {
+			cout << s.size() << "\n";
+		}
+		else if (a == "empty") {
+			if (s.size() == 0) {
+				cout << 1 << "\n";
+			}
+			else {
+				cout << 0 << "\n";
+			}
+		}
+		else if (a == "top") {
+			if (s.size() == 0) {
+				cout << -1 << "\n";
+			}
+			else {
+				cout << s.top() << "\n";
+			}
+		}
+	}
+
+
+	return 0;
+}
+
---
+++ 백준 스터디/백준_1158/백준_1158/백준_1158.cpp
@@ -0,0 +1,36 @@
+﻿// 백준_1158.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <queue>
+
+using namespace std;
+queue<int>q;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+
+	cout << "<";
+
+	while (!q.empty()) {
+		if (q.size() == 1) {
+			cout << q.front() << ">\n";
+			break;
+		}
+		for (int i = 1; i < k; i++) {
+			int tmp = q.front();
+			q.pop(); //앞에서 빼서
+			q.push(tmp); //뒤에 다시 넣는다
+		}
+		cout << q.front() << ", ";
+		q.pop();
+	}
+}
